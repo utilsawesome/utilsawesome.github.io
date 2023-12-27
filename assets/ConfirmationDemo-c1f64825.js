6fb46525f9aefebbfbc887cbcfbb8b15df2414f5
tuhalang <tuhalang007@outlook.com.vn>
2023-12-27 21:16:22 +0700

    init project

---
+++ assets/ConfirmationDemo-c1f64825.js
@@ -0,0 +1,1 @@
+import{bA as t,c as o,d as s,e}from"./index-db496ab7.js";const n={},c={class:"flex align-items-center py-5 px-3"},i=e("i",{class:"pi pi-fw pi-check mr-2 text-2xl"},null,-1),a=e("p",{class:"m-0 text-lg"},"Confirmation Component Content via Child Route",-1),r=[i,a];function _(l,d){return o(),s("div",c,r)}const p=t(n,[["render",_]]);export{p as default};
---
+++ assets/PersonalDemo-a66994a7.js
@@ -0,0 +1,1 @@
+import{bA as t,c as s,d as o,e}from"./index-db496ab7.js";const n={},c={class:"flex align-items-center py-5 px-3"},a=e("i",{class:"pi pi-fw pi-user mr-2 text-2xl"},null,-1),r=e("p",{class:"m-0 text-lg"},"Personal Component Content via Child Route",-1),i=[a,r];function l(_,d){return s(),o("div",c,i)}const m=t(n,[["render",l]]);export{m as default};
